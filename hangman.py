82a44a65ff73845c9ee5ac527abe862451619be9
shargrow <74335582+shargrow@users.noreply.github.com>
2021-02-25 20:22:37 -0700

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,43 @@
+import random
+
+def hangman():
+
+    hangman_words = ["hangman", "happy", "join", "game", "crazy", "top", "bottom"]
+    random_number = random.randint(0, 4)
+    word = hangman_words[random_number]
+    wrong_guesses = 0
+
+    stages = ["",
+             "_________          ",
+             "|                  ",
+             "|         |        ",
+             "|         0        ",
+             "|        /|\       ",
+             "|        / \       ",
+             "|                   "
+             ]
+    remaining_letters = list(word)
+    letter_board = ["__"] * len(word)
+    win = False
+    print("Wlecome to Hangman")
+
+    while wrong_guesses < len(stages) - 1:
+        print("\n")
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((" ".join(letter_board)))
+        print("\n".join(stages[0: wrong_guesses + 1]))
+        if "__" not in letter_board:
+            print("You win! The word was:")
+            print(" ".join(letter_board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong_guesses]))
+        print("You lose! The word was {}".format(word))
+hangman()
